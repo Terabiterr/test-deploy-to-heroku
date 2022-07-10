e79a2ecba012aabce177367a1cf7ba4fa7aef167
Terabiterr <brolerbrat@gmail.com>
2022-07-11 00:55:29 +0300

    fdss

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 const port = process.env.PORT || 8080;
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.get('/', (req, res) => {
+  res.send('Main page!!!');
 });

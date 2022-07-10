2a4d48aeb45d19b3b7fe4eb6f44f4e5e51f08130
Terabiterr <brolerbrat@gmail.com>
2022-07-11 00:51:24 +0300

    deploy2

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.get('/', function(req, res) {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
-
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
